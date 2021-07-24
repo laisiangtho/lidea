6c171f7eeee429348eec378e72dde2c9bfc8b1ec
Khen Solomon Lethil <khensolomon@gmail.com>
2021-07-24 08:05:15 +0200

    add Com

--- lib/extension.dart
+++ lib/extension.dart
@@ -4,8 +4,8 @@ extension LideaStringExtension on String {
   String bracketsHack() {
     return this.replaceAllMapped(RegExp(r'\[(.*?)\]'), (Match i) => i.group(1).toString().split('').reversed.join());
   }
-  String gitHack() {
-    return this.replaceFirst('git+http', 'http').replaceFirst('git+', '[moc.tnetnocresubuhtig.war//:sptth]').bracketsHack();
+  String gitHack({String? url}) {
+    return this.replaceFirst('git+http', 'http').replaceFirst('com+', url??"").replaceFirst('git+', '[moc.tnetnocresubuhtig.war//:sptth]').bracketsHack();
     // moc.tnetnocresubuhtig.war//:sptth
     // moc.buhtig//:sptth
   }
