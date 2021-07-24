4861864ce552d9b3bfb26abfe57d4e76094d6302
Khen Solomon Lethil <khensolomon@gmail.com>
2021-07-24 08:04:58 +0200

    add Notify

---
+++ lib/notify.dart
@@ -0,0 +1,15 @@
+library lidea;
+
+import 'package:flutter/foundation.dart';
+
+abstract class Notify with ChangeNotifier {
+  void notifyIf<T>(T element, T value) {
+    if (value != element){
+      notify();
+    }
+  }
+
+  void notify() {
+    notifyListeners();
+  }
+}
