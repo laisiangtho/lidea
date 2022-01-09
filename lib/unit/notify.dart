2759a64fd3daa66004f1a296792cae5bb46f417e
Khen Solomon Lethil <khensolomon@gmail.com>
2022-01-09 17:25:08 +0100

    Improve Notify

---
+++ lib/unit/notify.dart
@@ -0,0 +1,13 @@
+library lidea;
+
+import 'package:flutter/foundation.dart';
+
+abstract class Notify with ChangeNotifier {
+  void notifyIf<T>(T element, T value) {
+    if (value != element) {
+      notify();
+    }
+  }
+
+  void notify() => notifyListeners();
+}
