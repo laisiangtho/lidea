8773b851013d25e928ba280d1bea5b49a0ee3a9e
Khen Solomon Lethil <khensolomon@gmail.com>
2022-01-09 17:28:14 +0100

    Improve ClusterDocket

--- lib/cluster/docket.dart
+++ lib/cluster/docket.dart
@@ -1,6 +1,9 @@
+// import 'package:flutter/foundation.dart';
+
 import "package:lidea/hive.dart";
-import "package:lidea/type.dart";
-import "package:lidea/engine.dart";
+import "package:lidea/extension.dart";
+import "package:lidea/type/main.dart";
+import "package:lidea/util/main.dart";
 
 abstract class ClusterDocket {
   bool requireInitialized = true;
@@ -13,18 +16,16 @@ abstract class ClusterDocket {
 
   // final time = watch..start(); time.elapsedMilliseconds
   // final Stopwatch watch = new Stopwatch();
-
-  Future<void> ensureInitialized(Function? more) async {
+  /// Initiate primary context
+  Future<void> ensureInitialized() async {
     await Hive.initFlutter();
     Hive.registerAdapter(SettingAdapter());
     Hive.registerAdapter(PurchaseAdapter());
     Hive.registerAdapter(RecentSearchAdapter());
-    if (more != null) {
-      await more.call();
-    }
   }
 
-  Future<void> prepareInitialized(Function? more) async {
+  /// Prepare necessary context
+  Future<void> prepareInitialized() async {
     env = EnvironmentType.fromJSON(
       UtilDocument.decodeJSON<Map<String, dynamic>>(
         await UtilDocument.loadBundleAsString('env.json'),
@@ -57,10 +58,6 @@ abstract class ClusterDocket {
 
     boxOfRecentSearch = await Hive.openBox<RecentSearchType>('recent-search');
     // await collection.boxOfRecentSearch.clear();
-
-    if (more != null) {
-      await more.call();
-    }
   }
 
   Future<void> tokenUpdate({bool force = false, String file = 'token.json'}) {
@@ -90,12 +87,6 @@ abstract class ClusterDocket {
     }
   }
 
-  // String get suggestQuery => '';
-  // set suggestQuery(String ord) {
-  //   if (suggestQuery != ord) {
-  //     suggestQuery = ord;
-  //   }
-  // }
   String suggestQuery = '';
 
   double get fontSize => setting.fontSize;
@@ -105,30 +96,61 @@ abstract class ClusterDocket {
     }
   }
 
+  String get locale => setting.locale;
+  set locale(String locale) {
+    if (setting.locale != locale) {
+      settingUpdate(setting.copyWith(locale: locale));
+    }
+  }
+
   bool stringCompare(String? a, String b) => a!.toLowerCase() == b.toLowerCase();
+
+  String screenName(String? str) {
+    return str!.removeNonAlphanumeric().toTitleCase(joiner: '');
+  }
+
+  String screenClass(String? str) {
+    return str!.removeNonAlphanumeric().toTitleCase(joiner: '') + 'State';
+  }
 
   // boxOfRecentSearch addWordHistory
   // bool hasNotHistory(String ord) => this.boxOfRecentSearch.values.firstWhere((e) => stringCompare(e,ord),orElse: ()=>'') == null;
   // bool hasNotHistory(String ord) => this.boxOfRecentSearch.values.firstWhere((e) => stringCompare(e,ord),orElse: () => '')!.isEmpty;
 
-  MapEntry<dynamic, PurchaseType> boxOfPurchaseExist(String id) {
-    return boxOfPurchase.toMap().entries.firstWhere(
-          (e) => stringCompare(e.value.purchaseId, id),
-          orElse: () => MapEntry(null, PurchaseType()),
-        );
-  }
+  // MapEntry<dynamic, PurchaseType> boxOfPurchaseExistPurchaseId(String? id) {
+  //   return boxOfPurchase.toMap().entries.firstWhere(
+  //         (e) => stringCompare(e.value.purchaseId, id!),
+  //         orElse: () => MapEntry(null, PurchaseType()),
+  //       );
+  // }
 
-  bool boxOfPurchaseDeleteByPurchaseId(String id) {
-    if (id.isNotEmpty) {
-      final purchase = boxOfPurchaseExist(id);
-      if (purchase.key != null) {
-        // this.boxOfRecentSearch.deleteAt(history.key);
-        boxOfPurchase.delete(purchase.key);
-        return true;
-      }
-    }
-    return false;
-  }
+  // bool boxOfPurchaseDeleteByPurchaseId(String id) {
+  //   if (id.isNotEmpty) {
+  //     final purchase = boxOfPurchaseExistPurchaseId(id);
+  //     if (purchase.key != null) {
+  //       // this.boxOfRecentSearch.deleteAt(history.key);
+  //       boxOfPurchase.delete(purchase.key);
+  //       return true;
+  //     }
+  //   }
+  //   return false;
+  // }
+
+  // void boxOfPurchaseInsert({
+  //   required String productId,
+  //   required String purchaseId,
+  //   bool? completePurchase,
+  //   required String transactionDate,
+  //   bool? consumable,
+  // }) {
+  //   boxOfPurchase.add(PurchaseType(
+  //     productId: productId,
+  //     purchaseId: purchaseId,
+  //     completePurchase: completePurchase,
+  //     transactionDate: transactionDate,
+  //     consumable: consumable,
+  //   ));
+  // }
 
   // NOTE: History
   /// get all recentSearches
@@ -136,7 +158,7 @@ abstract class ClusterDocket {
     return boxOfRecentSearch.toMap().entries;
   }
 
-  /// recentSearch Exist of word
+  /// recentSearch is EXIST by word
   MapEntry<dynamic, RecentSearchType> recentSearchExist(String ord) {
     return recentSearches.firstWhere(
       (e) => stringCompare(e.value.word, ord),
@@ -144,7 +166,7 @@ abstract class ClusterDocket {
     );
   }
 
-  /// recentSearch Update item if exist, if not insert
+  /// recentSearch UPDATE on exist, if not INSERT
   bool recentSearchUpdate(String ord) {
     if (ord.isNotEmpty) {
       final ob = recentSearchExist(ord);
@@ -160,7 +182,7 @@ abstract class ClusterDocket {
     return false;
   }
 
-  /// recentSearch Delete item by word
+  /// recentSearch DELETE by word
   bool recentSearchDelete(String ord) {
     if (ord.isNotEmpty) {
       final ob = recentSearchExist(ord);
@@ -173,13 +195,6 @@ abstract class ClusterDocket {
   }
 
   Iterable<MapEntry<dynamic, RecentSearchType>> recentSearch() {
-    // if (searchQuery.isEmpty) {
-    //   return recentSearches;
-    // } else {
-    //   return recentSearches.where(
-    //     (e) => e.value.word.toLowerCase().startsWith(searchQuery.toLowerCase()),
-    //   );
-    // }
     return recentSearches;
   }
 
@@ -187,4 +202,61 @@ abstract class ClusterDocket {
   // void boxOfRecentSearchClear() {
   //   boxOfRecentSearch.clear();
   // }
+
+  // int _rsIndex(String word) {
+  //   return boxOfRecentSearch.toMap().values.toList().indexWhere((e) => e.word == word);
+  // }
+
+  // Future<void> _rsDelete(int index) => boxOfRecentSearch.deleteAt(index);
+  // Future<void> favoriteSwitch(String word) {
+  //   final index = _rsIndex(word);
+  //   if (index >= 0) {
+  //     return _rsDelete(index);
+  //   } else {
+  //     return boxOfRecentSearch.add(
+  //       RecentSearchType(
+  //         word: word,
+  //         date: DateTime.now(),
+  //       ),
+  //     );
+  //   }
+  // }
+
+  /// translate content from env,
+  /// if require localeCode can be provided as second param,
+  /// it is always withdraw to default if no match
+  String language(String text, {String? localeCode}) {
+    // localeName ?? Intl.systemLocale;
+    final lN0 = localeCode ?? locale;
+    final lN1 = 'en';
+
+    final l01 = env.language.entries;
+    final l02 = l01.first;
+
+    final l20 = l01.firstWhere((e) => e.key == lN0, orElse: () => l02);
+
+    final l21 = l20.value.entries.firstWhere(
+      (e) => e.key == text,
+      orElse: () {
+        if (lN0 == lN1) {
+          // Select language and default the same
+          return MapEntry('', text);
+        } else {
+          // No match in selected language
+          return l02.value.entries.firstWhere(
+            (e) => e.key == text,
+            orElse: () => MapEntry('', text),
+          );
+        }
+      },
+    );
+
+    return l21.value;
+  }
+
+  // void tmpInterpret() {
+  //   language('offlineaccess');
+  //   language('offlineaccess', localeCode: 'en');
+  // }
+
 }
