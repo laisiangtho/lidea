8bc9490e40b31d438c00feb6e37dc1f0ea4cff68
Khen Solomon Lethil <khensolomon@gmail.com>
2022-01-09 17:25:43 +0100

    Add Navigation
    - UnitNavigationNotify
    - UnitNavigationObserver

---
+++ lib/unit/navigation.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/widgets.dart';
+import 'package:lidea/unit/notify.dart';
+
+class UnitNavigationObserver extends NavigatorObserver {
+  UnitNavigationObserver(this._changeNotifier);
+
+  final UnitNavigationNotify _changeNotifier;
+
+  @override
+  void didPush(Route<dynamic> route, Route<dynamic>? previousRoute) {
+    Future.microtask(() {
+      _changeNotifier.push(route, previousRoute);
+    });
+  }
+
+  @override
+  void didPop(Route<dynamic> route, Route<dynamic>? previousRoute) {
+    _changeNotifier.pop(route, previousRoute);
+  }
+}
+
+class UnitNavigationNotify extends Notify {
+  String? name = '/';
+  int _index = 0;
+
+  int get index => _index;
+  set index(int value) {
+    notifyIf<int>(index, _index = value);
+  }
+
+  //  route.settings.name
+  void push(Route<dynamic> current, Route<dynamic>? previous) {
+    if (current.settings.name != null) {
+      name = current.settings.name!;
+      // debugPrint('push current ${current.settings.name}');
+
+    }
+    // if (previous != null) {
+    //   debugPrint('push previous ${previous.settings.name}');
+    // }
+    notify();
+  }
+
+  void pop(Route<dynamic> current, Route<dynamic>? previous) {
+    name = previous!.settings.name!;
+    // debugPrint('pop current ${current.settings.name} previous ${previous.settings.name}');
+    notify();
+  }
+}
