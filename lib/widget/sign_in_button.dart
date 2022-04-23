e662de0641409625526c4537bf8fd9f0f6208d87
Khen Solomon Lethil <khensolomon@gmail.com>
2022-04-23 08:01:07 +0200

    add SignInButton

---
+++ lib/widget/sign_in_button.dart
@@ -0,0 +1,41 @@
+part of 'main.dart';
+
+class SignInButton extends StatelessWidget {
+  final IconData? icon;
+  final String? label;
+  final void Function()? onPressed;
+  const SignInButton({
+    Key? key,
+    this.icon,
+    this.label,
+    this.onPressed,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return WidgetButton(
+      // margin: const EdgeInsets.all(7),
+      margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+      padding: const EdgeInsets.symmetric(horizontal: 7, vertical: 10),
+      // decoration: BoxDecoration(
+      //   color: Theme.of(context).primaryColor,
+      //   borderRadius: BorderRadius.circular(100),
+      //   border: Border.all(
+      //     color: Theme.of(context).shadowColor,
+      //     width: 1,
+      //   ),
+      // ),
+      elevation: 1.5,
+      borderRadius: const BorderRadius.all(Radius.circular(100)),
+      color: Theme.of(context).primaryColor,
+      child: WidgetLabel(
+        icon: icon,
+        iconColor: Theme.of(context).hintColor,
+        iconSize: 20,
+        label: label,
+        labelStyle: Theme.of(context).textTheme.labelLarge,
+      ),
+      onPressed: onPressed,
+    );
+  }
+}
