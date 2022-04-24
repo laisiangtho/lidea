9f977ddf62c113be2c006d5d9f5828733ce6e7ee
Khen Solomon Lethil <khensolomon@gmail.com>
2022-04-24 14:37:59 +0200

    add cached_network_image

--- lib/widget/main.dart
+++ lib/widget/main.dart
@@ -5,6 +5,10 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/gestures.dart';
+// import 'package:lidea/extension.dart';
+import 'package:lidea/cached_network_image.dart';
+import 'package:lidea/cluster/main.dart';
+import 'package:lidea/unit/authentication.dart';
 
 part 'builder.dart';
 part 'mark.dart';
@@ -13,6 +17,10 @@ part 'button.dart';
 part 'appbar.dart';
 
 part 'sign_in_button.dart';
+part 'user_icon.dart';
+part 'user_picture.dart';
+part 'user_theme.dart';
+part 'user_locale.dart';
 part 'confirm.dart';
 part 'popup.dart';
 part 'highlight.dart';
